219d4a405b35d2f1601000a6ca3bb0b23af2da0b
Aseman <masood1528014@gmail.com>
2025-09-07 12:28:00 +0330

    Auto commit v1

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,13 @@
-import os, sys
-BASE = os.path.dirname(__file__)
-if BASE not in sys.path:
-    sys.path.insert(0, BASE)
+# passenger_wsgi.py
+import sys, os
 
-from app import app as application
+# مسیر پروژه روی هاست (دقیقاً همونی که تو cPanel زدی)
+project_home = '/home/bztypmws/p-plus'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# ایمپورت اپلیکیشن از app.py
+from app import create_app
+
+# Passenger دنبال این متغیر می‌گرده
+application = create_app()
