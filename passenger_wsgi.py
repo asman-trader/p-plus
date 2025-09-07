46bb9d4e42ba750a50b4c1ce0a5fd4279b5b4242
Aseman <masood1528014@gmail.com>
2025-09-07 12:55:18 +0330

    Auto commit v1

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,10 @@
-# passenger_wsgi.py
-import sys, os
+import os
+import sys
 
-# مسیر پروژه روی هاست (دقیقاً همونی که تو cPanel زدی)
-project_home = '/home/bztypmws/p-plus'
+# مسیر پروژه روی هاست
+project_home = '/home/bztypmws/my-app'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# ایمپورت اپلیکیشن از app.py
-from app import create_app
-
-# Passenger دنبال این متغیر می‌گرده
-application = create_app()
+# ایمپورت اپ Flask از app.py
+from app import app as application
